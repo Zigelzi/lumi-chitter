47de4b530f73632e3240fa58077043604cdba401
Miika Savela <miika.a.savela@gmail.com>
2023-01-15 08:40:13 +0200

    Add empty state

--- src/lib/components/Chits/AllChits.svelte
+++ src/lib/components/Chits/AllChits.svelte
@@ -16,7 +16,27 @@
 </script>
 
 <div class="all-chits">
-	{#each allChits as chit (chit.id)}
-		<Chit {chit} />
-	{/each}
+	{#if allChits.length > 0}
+		{#each allChits as chit (chit.id)}
+			<Chit {chit} />
+		{/each}
+	{:else}
+	<div class="no-chits">
+		<h2>It's very calm in here...</h2>
+		<p>It seems that there's no chits currently. Write one above!</p>
+	</div>
+		
+	{/if}
 </div>
+<style>
+	.no-chits {
+		margin-top: 30px;
+		padding: 30px 20px 80px 20px;
+		background: #282c34;
+		box-shadow: 0px 0px 20px #1d2025;
+	}
+
+	h2	{
+		margin-bottom: 20px;
+	}
+</style>
