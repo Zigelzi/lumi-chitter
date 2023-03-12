242fcb2e4f4c8dce8a2467fcc3a8373c03e0f379
root <root@DESKTOP-SE242U8>
2023-03-12 19:20:01 +0200

    Add posting chit to BE

--- src/routes/api/chit/+server.ts
+++ src/routes/api/chit/+server.ts
@@ -1,18 +1,28 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({fetch}) => {
+export const GET: RequestHandler = async ({ fetch }) => {
 	const response = await fetch('http://localhost:5000/chit');
 
 	const data = await response.json();
 	return json(data, {
 		status: 200
-	})
-}
+	});
+};
 
-export const POST:RequestHandler = async ({request}) => {
-	const data = await request.json();
-	console.log(data);
+export const POST: RequestHandler = async ({ request }) => {
+	const chit = await request.json();
+	const settings = {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(chit)
+	};
+	const response = await fetch('http://localhost:5000/chit', settings);
+	const data = await response.json();
+	// console.log(data);
 
-	return json({ success: true });
+	return json({ success: true, data });
 };
