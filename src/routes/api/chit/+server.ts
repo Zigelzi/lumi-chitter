c023b1a4e974a2231b5d2cc7a36052bc7fc59798
Miika Savela <miika.a.savela@gmail.com>
2023-03-22 19:27:27 +0200

    Add fetching chits from backend

--- src/routes/api/chit/+server.ts
+++ src/routes/api/chit/+server.ts
@@ -1,8 +1,17 @@
 import { json } from '@sveltejs/kit';
-import type { RequestEvent } from './$types';
+import type { RequestHandler } from './$types';
 
-export const POST = async (event: RequestEvent) => {
-	const data = await event.request.json();
+export const GET: RequestHandler = async ({fetch}) => {
+	const response = await fetch('http://localhost:5000/chit');
+
+	const data = await response.json();
+	return json(data, {
+		status: 200
+	})
+}
+
+export const POST:RequestHandler = async ({request}) => {
+	const data = await request.json();
 	console.log(data);
 
 	return json({ success: true });
